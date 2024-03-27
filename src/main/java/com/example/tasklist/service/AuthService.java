83a18ddfb72121a77d2553e0c9a30008910058aa
Tabatskiy <coldfire347@gmail.com>
2024-03-27 21:27:40 +0200

    create project, creating project architecture, part 1

---
+++ src/main/java/com/example/tasklist/service/AuthService.java
@@ -0,0 +1,9 @@
+package com.example.tasklist.service;
+
+public interface AuthService {
+//
+//    JwtResponse login(JwtRequest loginRequest);
+//
+//    JwtResponse refresh(String refreshToken);
+
+}
